31f82831f39326e6e11cc87a0bffdac5b67a99bd
Tanza3D <archie@untone.uk>
2024-12-10 18:20:29 +0000

    patience

--- src/subscriptionProcessers/protogenProcessor.ts
+++ src/subscriptionProcessers/protogenProcessor.ts
@@ -2,125 +2,123 @@ import { FurryHelper } from '../furryhelper'
 import { FirehoseSubscription } from '../subscription'
 import colours from '../colours'
 
-export async function ProtogenProcessor(ops, subscription : FirehoseSubscription, logger : Function) {
+export async function ProtogenProcessor(ops, subscription: FirehoseSubscription, logger: Function) {
   const postsToDelete = ops.posts.deletes.map((del) => del.uri)
-  const postsToCreateWithFilter = await Promise.all(
-    ops.posts.creates.map(async (create) => {
-      try {
-        const endTime = new Date()
-        const startTime = new Date(create.record.createdAt)
-        const difference = endTime.getTime() - startTime.getTime() // This will give difference in milliseconds
-        const resultInMinutes = Math.round(difference / 60000)
-        const resultInSeconds = Math.round(difference / 1000) // Convert to seconds
+  const postsToCreateWithFilter = []
+  for (const create of ops.posts.creates) {
+    try {
+      const endTime = new Date()
+      const startTime = new Date(create.record.createdAt)
+      const difference = endTime.getTime() - startTime.getTime() // This will give difference in milliseconds
+      const resultInMinutes = Math.round(difference / 60000)
+      const resultInSeconds = Math.round(difference / 1000) // Convert to seconds
 
-        let blocked = false;
-        if(resultInSeconds > 999999) {
-          blocked = true;
-        }
-        else if (resultInSeconds > 60) {
-          logger('running ' + resultInSeconds + ' seconds behind (' + resultInMinutes + ' mins)')
-        }
+      let blocked = false
+      if (resultInSeconds > 999999) {
+        blocked = true
+      } else if (resultInSeconds > 60) {
+        logger('running ' + resultInSeconds + ' seconds behind (' + resultInMinutes + ' mins)')
+      }
 
-        let add = false
-        let reprocess_user = false
+      let add = false
+      let reprocess_user = false
 
 
-        let [user] = await subscription.db.execute('SELECT * FROM users WHERE did = ?', [create.author])
+      let [user] = await subscription.db.execute('SELECT * FROM users WHERE did = ?', [create.author])
 
-        //if (FurryHelper.isFurry(create.record.text).length > 0) logger(FurryHelper.isFurry(create.record.text))
-        // @ts-ignore
-        if (user.length < 1) {
-          const isfurry = FurryHelper.isFurry(create.record.text)
-          let extra = false
-          if (FurryHelper.isProtogen(create.record.text)) extra = true
-          if (isfurry.length > 0 || extra) {
-            reprocess_user = true
-            logger(colours.FgLightBlue + 'new furry ' + create.author + ' on matching ' + colours.FgGreen + isfurry.join(', '))
-          } else {
-            if (create.author.includes('3uyxuzj')) logger('!!!!!!!!! ' + create.author + ' is nota furry')
-          }
+      //if (FurryHelper.isFurry(create.record.text).length > 0) logger(FurryHelper.isFurry(create.record.text))
+      // @ts-ignore
+      if (user.length < 1) {
+        const isfurry = FurryHelper.isFurry(create.record.text)
+        let extra = false
+        if (FurryHelper.isProtogen(create.record.text)) extra = true
+        if (isfurry.length > 0 || extra) {
+          reprocess_user = true
+          logger(colours.FgLightBlue + 'new furry ' + create.author + ' on matching ' + colours.FgGreen + isfurry.join(', '))
         } else {
-          if (user[0]['protogen'] == 1) {
-            add = true
-            reprocess_user = false
-          }
-          if(user[0]['blocked'] == 1) {
-            blocked = true;
-          }
+          if (create.author.includes('3uyxuzj')) logger('!!!!!!!!! ' + create.author + ' is nota furry')
         }
-        if(create.record.text.toLowerCase().replace("'", "").includes("im a protogen!")) reprocess_user = true;
-
-        if (reprocess_user == true) {
-          const isfurryx: boolean = (FurryHelper.isFurry(create.record.text).length > 0)
-          const profile = await subscription.getUserData(create.author)
-
-          logger('reprocessing ' + profile.data.handle)
-          let protogen = false
-          if (FurryHelper.isProtogen(profile.data.displayName)) protogen = true
-          if (FurryHelper.isProtogenStrict(profile.data.description)) protogen = true
-          if (FurryHelper.isProtogenTag(profile.data.handle)) protogen = true
-          if (FurryHelper.isProtogen(profile.data.handle)) protogen = true
-
-          if (protogen) add = true
-
-          if (protogen) logger('that\'s a new protogen :D - ' + profile.data.handle)
-
-          const data = {
-            'user': create.author,
-            'furry': isfurryx,
-            'protogen': protogen,
-          }
-
-          await subscription.db.execute('REPLACE INTO `users` (`did`, `furry`, `protogen`)\n' +
-            'VALUES (?, ?, ?);', [data.user, data.furry ? 1 : 0, data.protogen ? 1 : 0])
+      } else {
+        if (user[0]['protogen'] == 1) {
+          add = true
+          reprocess_user = false
         }
-
-        const textprotogen = FurryHelper.isProtogen(create.record.text)
-        if (textprotogen) add = true
-
-        if (create.record?.reply && add) {
-          add = false; // no longer care about replies
-        }
-
-
-        var isArt = FurryHelper.isArt(create.record.text);
-        if (!(create.record.embed && create.record.embed.$type == "app.bsky.embed.images")) {
-          isArt = false;
-        }
-
-        if(isArt && add) {
-          console.log("this is protogen art!", create.record)
-        }
-
-        if(create.record.text.toLowerCase().includes("gta6trailer")) add = false;
-
-        if(blocked) add = false;
-
-        if (add) logger('adding ; ' + create.record.text)
-
-        return {
-          shouldCreate: add,
-          post: {
-            uri: create.uri,
-            cid: create.cid,
-            indexedAt: new Date().toISOString(),
-            art: isArt ? 1 : 0
-          },
-        }
-      } catch (e) {
-        logger(e);
-        return {
-          shouldCreate: false,
-          post: {
-            uri: "",
-            cid: "",
-            indexedAt: new Date().toISOString(),
-            art: 0
-          },
+        if (user[0]['blocked'] == 1) {
+          blocked = true
         }
       }
-    }),
-  )
+      if (create.record.text.toLowerCase().replace('\'', '').includes('im a protogen!')) reprocess_user = true
+
+      if (reprocess_user == true) {
+        const isfurryx: boolean = (FurryHelper.isFurry(create.record.text).length > 0)
+        const profile = await subscription.getUserData(create.author)
+
+        logger('reprocessing ' + profile.data.handle)
+        let protogen = false
+        if (FurryHelper.isProtogen(profile.data.displayName)) protogen = true
+        if (FurryHelper.isProtogenStrict(profile.data.description)) protogen = true
+        if (FurryHelper.isProtogenTag(profile.data.handle)) protogen = true
+        if (FurryHelper.isProtogen(profile.data.handle)) protogen = true
+
+        if (protogen) add = true
+
+        if (protogen) logger('that\'s a new protogen :D - ' + profile.data.handle)
+
+        const data = {
+          'user': create.author,
+          'furry': isfurryx,
+          'protogen': protogen,
+        }
+
+        await subscription.db.execute('REPLACE INTO `users` (`did`, `furry`, `protogen`)\n' +
+          'VALUES (?, ?, ?);', [data.user, data.furry ? 1 : 0, data.protogen ? 1 : 0])
+      }
+
+      const textprotogen = FurryHelper.isProtogen(create.record.text)
+      if (textprotogen) add = true
+
+      if (create.record?.reply && add) {
+        add = false // no longer care about replies
+      }
+
+
+      var isArt = FurryHelper.isArt(create.record.text)
+      if (!(create.record.embed && create.record.embed.$type == 'app.bsky.embed.images')) {
+        isArt = false
+      }
+
+      if (isArt && add) {
+        console.log('this is protogen art!', create.record)
+      }
+
+      if (create.record.text.toLowerCase().includes('gta6trailer')) add = false
+
+      if (blocked) add = false
+
+      if (add) logger('adding ; ' + create.record.text)
+
+      postsToCreateWithFilter.push({
+        shouldCreate: add,
+        post: {
+          uri: create.uri,
+          cid: create.cid,
+          indexedAt: new Date().toISOString(),
+          art: isArt ? 1 : 0,
+        },
+      })
+    } catch (e) {
+      logger(e)
+      postsToCreateWithFilter.push({
+        shouldCreate: false,
+        post: {
+          uri: '',
+          cid: '',
+          indexedAt: new Date().toISOString(),
+          art: 0,
+        },
+      })
+    }
+  }
 
   const postsToCreate = postsToCreateWithFilter
     .filter(({ shouldCreate }) => shouldCreate)
@@ -132,8 +130,8 @@ export async function ProtogenProcessor(ops, subscription : FirehoseSubscription
 
     // Create the SQL query with placeholders
     const deleteQuery = `DELETE
-                             FROM post
-                             WHERE uri IN (${placeholders})`
+                         FROM post
+                         WHERE uri IN (${placeholders})`
 
     // Execute the query with the actual values
     await subscription.db.execute(deleteQuery, postsToDelete)
@@ -143,11 +141,11 @@ export async function ProtogenProcessor(ops, subscription : FirehoseSubscription
   if (postsToCreate.length > 0) {
     const values = postsToCreate.map(post => [post.uri, post.cid, post.indexedAt, post.art])
     const insertQuery = `
-            INSERT INTO post (uri, cid, indexedAt, art)
-            VALUES ?
-            ON DUPLICATE KEY UPDATE cid       = VALUES(cid),
-                                    indexedAt = VALUES(indexedAt)
-        `
+        INSERT INTO post (uri, cid, indexedAt, art)
+        VALUES ?
+        ON DUPLICATE KEY UPDATE cid       = VALUES(cid),
+                                indexedAt = VALUES(indexedAt)
+    `
     await subscription.db.query(insertQuery, [values])
   }
 }
