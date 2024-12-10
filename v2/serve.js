3b40fd452e150722ccc57803a25d4e83e5f626b6
Tanza3D <archie@untone.uk>
2024-12-10 20:07:48 +0000

    chance port

--- v2/serve.js
+++ v2/serve.js
@@ -2,7 +2,7 @@ import express from 'express'
 import CreateBlueskyRoutes from './utils/bluesky.js'
 
 const app = express()
-const port = 3000
+const port = 3001
 CreateBlueskyRoutes(app);
 
 app.listen(port, () => {
