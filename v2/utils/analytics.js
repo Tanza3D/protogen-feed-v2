c8a4106aad3269b0d65c1d68b6eda32febb19b52
Tanza3D <archie@untone.uk>
2025-03-26 01:21:09 +0000

    fix spam

--- v2/utils/analytics.js
+++ v2/utils/analytics.js
@@ -1,6 +1,18 @@
-import db from "./database.js";
+// Object to store the last time each user pushed an analytic
+const userLastPushTime = {};
 
 export async function PushAnalytic(user, feed) {
-    await db.execute(`INSERT INTO \`analytics\` (\`feed\`, \`user\`, \`time\`)
-VALUES (?, ?, now());`, [feed, user])
+    const currentTime = Date.now();
+    const oneMinuteAgo = 60 * 1000;
+
+    if (userLastPushTime[user] && currentTime - userLastPushTime[user] < oneMinuteAgo) {
+        return; // oh well
+    }
+
+    userLastPushTime[user] = currentTime;
+
+    await db.execute(`
+        INSERT INTO \`analytics\` (\`feed\`, \`user\`, \`time\`)
+        VALUES (?, ?, now());
+    `, [feed, user]);
 }
